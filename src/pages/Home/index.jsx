a58d4c06e42a08137adca9e9551b1924840ad514
João Henrique <joaohenrique7482@gmail.com>
2022-08-02 18:51:51 -0300

    refactor: folders and structuring

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import './home.css'
+
+export function Home(){
+    return (
+        <div>
+            <h1>Home</h1>
+            <input type="text" placeholder="Digite aqui sua mensagem..."/>
+        </div>
+    )
+}
