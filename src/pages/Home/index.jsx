7228e72019f822f8c73f2e67327c319f36447d14
João Henrique <joaohenrique7482@gmail.com>
2022-08-02 19:18:56 -0300

    feat: Add style

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,9 +2,10 @@ import './home.css'
 
 export function Home(){
     return (
-        <div>
-            <h1>Home</h1>
-            <input type="text" placeholder="Digite aqui sua mensagem..."/>
+        <div className='container'>
+            <h1>Lista de presença</h1>
+            <input type="text" placeholder="Digite aqui seu nome..."/>
+            <button>Adicionar</button>
         </div>
     )
 }
